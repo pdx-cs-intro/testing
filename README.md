41155c91a7d670ab64e61f6b9c752a8abbf8909e
Bart Massey <bart.massey@gmail.com>
2021-01-25 23:35:34 -0800

    triangle testing demo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Program Testing Example
+Bart Massey
+
+Versions of a triangle-calculation program as it goes
+through the process of testing and debugging.
---
+++ triangle-1.py
@@ -0,0 +1,16 @@
+# Print the number of balls for the largest triangle
+# that could be made with n balls lying around.
+# Bart Massey 2021
+
+# The triangle calculation.
+def triangle(n):
+	row_length = 1
+	balls_used = 0
+	while n > row_length:
+		n = n - row_length
+		balls_used = balls_used + row_length
+		row_length = row_length + 1
+	return balls_used
+
+n = int(input("number of balls? "))
+print(triangle(n))
