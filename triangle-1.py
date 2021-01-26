f9abd99cf0d60890a75bf41db0a45f69254e1194
Bart Massey <bart.massey@gmail.com>
2021-01-26 00:00:36 -0800

    replaced tabs with spaces in triangle-1

--- triangle-1.py
+++ triangle-1.py
@@ -3,14 +3,14 @@
 # Bart Massey 2021
 
 # The triangle calculation.
-def triangle(n):
-	row_length = 1
-	balls_used = 0
-	while n > row_length:
-		n = n - row_length
-		balls_used = balls_used + row_length
-		row_length = row_length + 1
-	return balls_used
+def triangle_loop(n):
+    row_length = 1
+    balls_used = 0
+    while n > row_length:
+        n = n - row_length
+        balls_used = balls_used + row_length
+        row_length = row_length + 1
+    return balls_used
 
 n = int(input("number of balls? "))
 print(triangle(n))
