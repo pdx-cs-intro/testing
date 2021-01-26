0a28194d1d215a8d4be2e19b84f9a6bfc371a7b0
Bart Massey <bart.massey@gmail.com>
2021-01-26 00:08:22 -0800

    fully commented triangle-oracle

--- triangle-oracle.py
+++ triangle-oracle.py
@@ -5,6 +5,9 @@
 import math
 
 # The triangle calculation.
+#
+# Add a row at a time to the triangle until there aren't
+# enough balls to make a new row.
 def triangle_loop(n):
     row_length = 1
     balls_used = 0
@@ -15,6 +18,19 @@ def triangle_loop(n):
     return balls_used
 
 # The other triangle calculation.
+#
+# Gauss's Sum: 1+2+…+k = k * (k - 1) / 2
+# So the largest number of rows that can be made is
+# the largest integer k such that
+#    
+#   k * (k - 1) / 2 <= n
+#
+# Using the quadratic formula, we find
+#
+#   k = floor(-1 + sqrt(1 + 8 * n) / 2)
+#
+# Substituting k back into Gauss's Sum, we then get
+# the number of balls in the triangle.
 def triangle_math(n):
         k = int(math.floor((math.sqrt(1 + 8 * n) - 1) / 2))
         return k * (k + 1) // 2
